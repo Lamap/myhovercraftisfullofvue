75c03fed2ab67fab84744da2886add48953dbe31
lenkei.akos <lenkei.akos@virgo.hu>
2019-06-28 13:02:16 +0200

    playing 1

---
+++ src/components/test/Test.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="test">
+    <h1 class="test__elek">Test {{testProp}}</h1>
+    <md-button>sajnálatos <md-icon>home</md-icon></md-button>
+    <md-autocomplete :value="bela" :md-options="jojoItems" />
+  </div>
+</template>
+<script>
+
+  export default {
+    props: {
+      testProp: {
+        type: String,
+        default: 'jenős'
+      }
+    },
+    data () {
+      return {
+        jojoItems: ['aha', 'nono', 'nana'],
+        bela: null
+      }
+    },
+    created () {
+      console.info('test component created')
+    }
+  }
+</script>
+<style lang="less">
+  @color-var: #0d3ddd;
+  .test {
+    &__elek {
+      color: @color-var;
+    }
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { MdButton, MdAutocomplete, MdField, MdMenu, MdList } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(MdButton);
+Vue.use(MdAutocomplete);
+Vue.use(MdField);
+Vue.use(MdMenu);
+Vue.use(MdList);
 
 new Vue({
   router,
