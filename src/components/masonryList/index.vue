bb626139b85b0bf5ec1e169f84fb979a1704675c
lenkei.akos <lenkei.akos@virgo.hu>
2019-07-10 12:52:36 +0200

    playing 2

---
+++ src/components/masonryList/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hvr-imagelist">
+    I am the hovercraft of walrusssss
+    <div class="hvr-imagelist__item" v-for="item in items" :key="item">
+      <slot name="list-item">
+      </slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'masonryList',
+  props: {
+    maxColWidth: {
+      type: Number,
+      default: 200
+    },
+    items: {
+      required: true,
+      type: Array
+    }
+  },
+  created () {
+    console.log('masonry list created hónaljBéla levelesJenőSercegésss')
+  },
+  methods: {
+    bottomReached () {
+      this.$emit('imagelist:bottom-reached')
+    }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,34 @@
-<template>
+<template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="My hovercraft is full"/>
+    <ImageList :items="items">
+      <template v-slot:list-item="slotProps"><div class="gggggg">kkkkk {{slotProps.item}}iii</div></template>
+    </ImageList>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import ImageList from '@/components/masonryList'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    HelloWorld,
+    ImageList
+  },
+  data () {
+    return {
+      items: [
+        'jenő',
+        'béla',
+        'ahossl'
+      ]
+    };
+  },
+  created () {
   }
 }
 </script>
